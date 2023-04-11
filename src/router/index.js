d8ffc3bfdec4b9abaa86944c537b944de03d8144
左风 <595563214@qq.com>
2023-04-11 20:11:49 +0800

    feat: 增加口令

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/login.vue'
 import HomeView from '../views/index.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView
     }
---
+++ src/views/login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="box">
+    <input class="input" v-model="answer" placeholder="输入你的口令" />
+    <!-- 写一个按钮 -->
+    <button class="button" @click="send">发送</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { Cloud } from "laf-client-sdk";
+
+const cloud = new Cloud({
+  baseUrl: "https://a3uvbk.laf.dev",
+  getAccessToken: () => "",
+});
+
+const answer = ref("");
+
+async function send() {
+  const message = answer.value;
+  answer.value = "";
+  if (message == "") {
+    return;
+  }
+  try {
+    const res = await cloud.invoke("get", { message });
+
+    if (res.ok) {
+      window.location.href = "/home";
+    }
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+}
+</script>
+
+<style>
+.box {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+}
+.input {
+  margin-top: 300px;
+  width: 200px;
+  height: 40px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 0 10px;
+}
+.button {
+  margin-top: 300px;
+  margin-left: 10px;
+  width: 60px;
+  height: 40px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background: #fff;
+  cursor: pointer;
+}
+</style>
